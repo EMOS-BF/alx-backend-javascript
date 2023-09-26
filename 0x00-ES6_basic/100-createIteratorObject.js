bda9f3a955c64ce00abecf2893622eaf5718c522
SOME <sourbar20@gmail.com>
2023-09-26 22:27:52 +0000

    Advance task

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+ let employees = []
+ let result = []
+	for (const value of Object.values(report.allEmployees)){
+	 employees.push(value);
+	}
+	for (const element of employees){
+		result = [...result, ...element]
+	}
+	return result;
+}
